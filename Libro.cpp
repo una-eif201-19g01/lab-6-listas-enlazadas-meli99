037acbcb3fad87892cb0d95e7eecfc8c6a6a7110
Meli <53483691+meli99@users.noreply.github.com>
2019-09-27 00:50:04 -0600

    Update Libro.cpp

--- Libro.cpp
+++ Libro.cpp
@@ -1,5 +1,53 @@
-//
-// Created by Maikol Guzman  on 2019-09-16.
-//
-
 #include "Libro.h"
+
+Libro::Libro(){	
+}
+
+Libro::Libro(const string &editorial,const string &titulo,const string &autor,int ano,int in) : titulo(titulo),autor(autor),editorial(editorial),anno(anno),isbn(isbn){
+}
+
+void Libro::setEditorial(const string &editorial){
+	Libro::editorial = editorial;
+}
+
+void Libro::setTitulo(const string &titulo){
+	Libro::titulo = titulo;
+}
+
+void Libro::setAutor(const string &autor){
+	Libro::autor = autor;
+}
+
+void Libro::setIn(int in){
+	Libro::in=in;
+}
+
+void Libro::setAno(int ano){
+	Libro::ano=ano;
+}
+
+const string &Libro::getEditorial()const{
+	return editorial;
+}
+
+const string &Libro::getTitulo()const{
+	return titulo;
+}
+
+const string &Libro::getAutor()const{
+	return autor;
+}
+
+int Libro::getIn()const{
+	return in;
+}
+
+int Libro::getAno()const{
+	return ano;
+}
+
+string Libro::toString(){
+	string reporte;
+	reporte=+"Titulo del libro: "+getAutor()+"\n"+"el autor del libro es:"+getAutor()+ "\n" +"el año de publicacion del libro es:" + to_string(getAnno()) + "\n" + "el codigo ISBN del libro es: " +to_string(getIn()) + "\n";
+	return reporte;
+}
